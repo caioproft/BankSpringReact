d349f3125656ecbf4c4afcd609c6122d351d4efc
Caio Silveira F de Proft <caio_proft@hotmail.com>
2020-01-24 18:53:08 -0300

    Criação da classe AccountServiceImplementation e refatoração da exeção InvalidValueException

--- src/main/java/com/invillia/bankSpringAndReact/service/implementation/AccountServiceImplementation.java
+++ src/main/java/com/invillia/bankSpringAndReact/service/implementation/AccountServiceImplementation.java
@@ -1,6 +1,7 @@
 package com.invillia.bankSpringAndReact.service.implementation;
 
 import com.invillia.bankSpringAndReact.excepctions.IdNotFoundException;
+import com.invillia.bankSpringAndReact.excepctions.InvalidValueException;
 import com.invillia.bankSpringAndReact.mapper.AccountMapper;
 import com.invillia.bankSpringAndReact.model.entity.Account;
 import com.invillia.bankSpringAndReact.model.entity.User;
@@ -37,6 +38,33 @@ public class AccountServiceImplementation implements AccountService {
     public void create(User user){
 
         Account account = new Account(user);
+        accountRepository.save(account);
+    }
+
+    @Transactional
+    public void withdraw(final Double value, final Account account){
+        if(value > 0){
+            if (account.getBalance() >= value){
+                account.setBalance(account.getBalance() - value);
+            }
+            else{
+                throw new InvalidValueException("Você não possui saldo suficiente!");
+            }
+        }
+        else {
+            throw new InvalidValueException("O valor do saque não pode ser negativo!");
+        }
+        accountRepository.save(account);
+    }
+
+    @Transactional
+    public void deposit (final Double value, final Account account){
+        if (value > 0){
+            account.setBalance(account.getBalance() + value);
+        }
+        else{
+            throw new InvalidValueException("Não é possível depositar um valor negativo!");
+        }
         accountRepository.save(account);
     }
 }
